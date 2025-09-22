3eef47add3aae0c35687bbb843958ec60133b17b
Aidan <aidanmichaelwoodcock@gmail.com>
2025-09-21 21:22:54 -0700

    Final commit end of coding session

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // COMSC-210 | Lab 11: Pointers II | Aidan Woodcock | 2025-09-21
 // IDE used: Visual Studio Code
-// Program to manage a car owner's registry using structures, pointers, and dynamic memory allocation, this could be useful for a car dealership or rental service.
+// Program to manage a car owner's registry using structures, pointers, and dynamic memory allocation
+// This could be useful for a car dealership or rental service.
 
 #include <iostream>
 #include <iomanip>
@@ -9,10 +10,11 @@
 using namespace std;
 
 const int REGISTRY_SIZE = 3; 
+
 // Structure to represent a car owner's registry
 struct carRegistry {
     string ownerName;    // Name of the car owner
-    string licensePlate; // License plate number, could have also been dynamic but kept it simple
+    string licensePlate; // License plate number, makes sense for it to also be a dynamic array, but kept it simple
     int numofCar;        // Number of cars owned
     string *carMake;     // Dynamic array to store car makes
 
@@ -25,7 +27,6 @@ struct carRegistry {
     }
 };
 
-
 // Function prototypes
 void inputRegistryData(carRegistry *regPtr);
 void displayRegistryData(carRegistry *regPtr);
@@ -33,30 +34,26 @@ void displayRegistryData(carRegistry *regPtr);
 int main() {
     // Create a dynamic array of carRegistry structs
     carRegistry *registry = new carRegistry[REGISTRY_SIZE];
-    
+
     // Input data for each registry entry
     for (int i = 0; i < REGISTRY_SIZE; i++) {
         inputRegistryData(&registry[i]);
     }
-    
     // Display all registry data
     for (int i = 0; i < REGISTRY_SIZE; i++) {
         displayRegistryData(&registry[i]);
     }
-    
     // Clean up memory by deleting the dynamic array
     delete[] registry;
     
     return 0;
 }
 
-// Function definitions
 // Function to input data for a car registry entry using pointers and dynamic memory allocation
 void inputRegistryData(carRegistry *regPtr) {
-    static int ownerCount = 1; // Static variable to track owner number
+    static int ownerCount = 1; // static variable as index
     cout << "Enter details for owner #" << ownerCount << ":\n";
     cout << "Owner's Name: ";
-    cin.ignore(); 
     getline(cin, regPtr->ownerName);
     cout << "License Plate: ";
     getline(cin, regPtr->licensePlate);
@@ -64,7 +61,8 @@ void inputRegistryData(carRegistry *regPtr) {
     cin >> regPtr->numofCar;
     // Create dynamic array for car makes based on number of cars
     regPtr->carMake = new string[regPtr->numofCar];
-    cin.ignore(); 
+    // Clear the input buffer after reading numofCar
+    cin.ignore();
     for(int j = 0; j < regPtr->numofCar; j++) {
         cout << "Car Make #" << (j + 1) << ": ";
         getline(cin, regPtr->carMake[j]);
@@ -73,16 +71,17 @@ void inputRegistryData(carRegistry *regPtr) {
     ownerCount++;
 }
 
+// Function to display data for a car registry entry
 void displayRegistryData(carRegistry *regPtr) {
-        static int i = 0;
-        cout << "Summary of Car Registry Entry: #" << (i + 1) << endl;
-        cout << "Owner #" << (i + 1) << ": " << endl;
-        cout << "Owner's Name: " << regPtr->ownerName << endl;
-        cout << "License Plate: " << regPtr->licensePlate << endl;
-        cout << "Number of Cars: " << regPtr->numofCar << endl;
-        for(int j = 0; j < regPtr->numofCar; j++) {
-            cout << "Car Make #" << (j + 1) << ": " << regPtr->carMake[j] << endl;
-        }
-        cout << endl << endl;
-        i++;
+    static int i = 0; // Static variable to track display count
+    cout << "Summary of Car Registry Entry: #" << (i + 1) << endl;
+    cout << "Owner #" << (i + 1) << ": " << endl;
+    cout << "Owner's Name: " << regPtr->ownerName << endl;
+    cout << "License Plate: " << regPtr->licensePlate << endl;
+    cout << "Number of Cars: " << regPtr->numofCar << endl;
+    for(int j = 0; j < regPtr->numofCar; j++) {
+        cout << "Car Make #" << (j + 1) << ": " << regPtr->carMake[j] << endl;
+    }
+    cout << endl << endl;
+    i++;
 }
